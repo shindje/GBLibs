d0122a760ba24d439b3628123fcefb8afe71f541
shindje <aleks.shindje@gmail.com>
2020-11-26 17:51:33 +0300

    RxJava

--- app/src/main/java/com/example/gblibs/mvp/presenter/MainPresenter.kt
+++ app/src/main/java/com/example/gblibs/mvp/presenter/MainPresenter.kt
@@ -2,6 +2,7 @@ package com.example.gblibs.mvp.presenter
 
 import com.example.gblibs.mvp.view.MainView
 import com.example.gblibs.navigation.Screens
+import com.example.gblibs.rxlearn.Operators
 import moxy.MvpPresenter
 import ru.terrakok.cicerone.Router
 
@@ -10,6 +11,8 @@ class MainPresenter(val router: Router): MvpPresenter<MainView>() {
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
         router.replaceScreen(Screens.UsersScreen())
+
+        Operators().exec()
     }
 
     fun backClick() {
